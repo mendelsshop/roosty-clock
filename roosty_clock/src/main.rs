36fceaecb42d3e72c01d280a44ff0e77b2c12bce
mendelsshop <mendelsshop@gmail.com>
2025-12-18 23:20:08 -0500

    starting to move the the alarm ringing and logic into its own dameon

--- roosty_clock/src/main.rs
+++ roosty_clock/src/main.rs
@@ -6,7 +6,14 @@
     clippy::missing_panics_doc
 )]
 
-use std::{collections::HashMap, error::Error, fs, io::Write, path::PathBuf, thread};
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs,
+    io::{BufReader, Write},
+    path::PathBuf,
+    thread,
+};
 
 use clap::{Parser, Subcommand};
 use eframe::{egui::ViewportBuilder, run_native};
@@ -15,6 +22,10 @@ use roosty_clock::{
     communication::{Message, MessageType},
     config::Config,
     Clock,
+};
+use {
+    interprocess::local_socket::{prelude::*, GenericFilePath, GenericNamespaced, Stream},
+    std::io::prelude::*,
 };
 
 #[derive(Parser)]
@@ -74,6 +85,39 @@ fn main() -> Result<(), Box<dyn Error>> {
         None => {}
     }
 
+    // Pick a name.
+    let name = if GenericNamespaced::is_supported() {
+        "roosty-clockd.sock".to_ns_name::<GenericNamespaced>()?
+    } else {
+        "/tmp/roosty-clockd.sock".to_fs_name::<GenericFilePath>()?
+    };
+
+    // Preemptively allocate a sizeable buffer for receiving. This size should be enough and
+    // should be easy to find for the allocator.
+    let mut buffer = String::with_capacity(128);
+
+    // Create our connection. This will block until the server accepts our connection, but will
+    // fail immediately if the server hasn't even started yet; somewhat similar to how happens
+    // with TCP, where connecting to a port that's not bound to any server will send a "connection
+    // refused" response, but that will take twice the ping, the roundtrip time, to reach the
+    // client.
+    let conn = Stream::connect(name)?;
+    // Wrap it into a buffered reader right away so that we could receive a single line out of it.
+    let mut conn = BufReader::new(conn);
+
+    // Send our message into the stream. This will finish either when the whole message has been
+    // sent or if a send operation returns an error. (`.get_mut()` is to get the sender,
+    // `BufReader` doesn't implement pass-through `Write`.)
+    conn.get_mut().write_all(b"Hello from client!\n")?;
+
+    // We now employ the buffer we allocated prior and receive a single line, interpreting a
+    // newline character as an end-of-file (because local sockets cannot be portably shut down),
+    // verifying validity of UTF-8 on the fly.
+    conn.read_line(&mut buffer)?;
+
+    // Print out the result, getting the newline for free!
+    print!("Server answered: {buffer}");
+    //{
     let (tx, rx) = std::sync::mpsc::channel();
     thread::spawn(move || {
         let stream_handle = rodio::OutputStreamBuilder::open_default_stream().unwrap();
---
+++ roosty_clockd/src/main.rs
@@ -0,0 +1,139 @@
+use chrono::NaiveTime;
+use interprocess::local_socket::{GenericNamespaced, ListenerOptions, Stream, prelude::*};
+use serde::{Deserialize, Serialize};
+use std::io::{self, BufReader, prelude::*};
+use std::path::PathBuf;
+use std::thread;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Alarm {
+    pub name: Option<String>,
+    #[serde(with = "toml_datetime_compat")]
+    pub time: NaiveTime,
+    pub volume: f32,
+    pub sound: u64,
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Sound {
+    pub name: String,
+    pub path: PathBuf,
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum AlarmEdit {
+    #[serde(with = "toml_datetime_compat")]
+    Time(NaiveTime),
+    Name(Option<String>),
+    Sound(u64),
+    Volume(f64),
+    Enable(bool),
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+enum ClientMessage {
+    GetAlarms,
+    SetAlarm(u64, AlarmEdit),
+    AddAlarm(Alarm),
+    RemoveAlarm(u64),
+    GetSounds(u64),
+    AdddSound(Sound),
+    RemoveSound(u64),
+    StopAlarm(u64),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum ServerMessage {
+    Alarms(Vec<()>),
+    AlarmSet(u64, AlarmEdit),
+    AlaramAdded(Alarm),
+    AlarmRemoved(u64),
+    Sounds(Vec<Sound>),
+    SoundAdded(Sound),
+    SoundRemoved(u64),
+    AlarmRinging(u64),
+    AlarmStopped(u64),
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum Alert {
+    AlarmSet(u64, AlarmEdit),
+    AlaramAdded(Alarm),
+    AlarmRemoved(u64),
+    SoundAdded(Sound),
+    SoundRemoved(u64),
+    AlarmRinging(u64),
+    AlarmStopped(u64),
+}
+fn main() -> std::io::Result<()> {
+    // Define a function that checks for errors in incoming connections. We'll use this to filter
+    // through connections that fail on initialization for one reason or another.
+    fn handle_error(conn: io::Result<Stream>) -> Option<Stream> {
+        match conn {
+            Ok(c) => Some(c),
+            Err(e) => {
+                eprintln!("Incoming connection failed: {e}");
+                None
+            }
+        }
+    }
+
+    // Pick a name.
+    let printname = "roosty-clockd.sock";
+    let name = printname.to_ns_name::<GenericNamespaced>()?;
+
+    // Configure our listener...
+    let opts = ListenerOptions::new().name(name);
+
+    // ...then create it.
+    let listener = match opts.create_sync() {
+        Err(e) if e.kind() == io::ErrorKind::AddrInUse => {
+            eprintln!(
+                "Error: could not start server because the socket file is occupied. Please check
+                if {printname} is in use by another process and try again."
+            );
+            return Err(e);
+        }
+        x => x?,
+    };
+
+    eprintln!("Server running at {printname}");
+
+    let (s, r) = crossbeam_channel::unbounded();
+    for conn in listener.incoming().filter_map(handle_error) {
+        let (s, r) = (s.clone(), r.clone());
+        thread::spawn(move || {
+            let (read, mut write) = conn.split();
+            let mut buffer = Vec::new();
+            // Wrap the connection into a buffered receiver right away
+            // so that we could receive a single line from it.
+            let mut conn = BufReader::new(read);
+            println!("Incoming connection!");
+
+            // Since our client example sends first, the server should receive a line and only then
+            // send a response. Otherwise, because receiving from and sending to a connection cannot
+            // be simultaneous without threads or async, we can deadlock the two processes by having
+            // both sides wait for the send buffer to be emptied by the other.
+            conn.read_to_end(&mut buffer).unwrap();
+            let message: ClientMessage = toml::from_slice(&buffer).unwrap();
+            match message {
+                ClientMessage::GetAlarms => todo!(),
+                ClientMessage::SetAlarm(_, alarm_edit) => todo!(),
+                ClientMessage::AddAlarm(alarm) => todo!(),
+                ClientMessage::RemoveAlarm(_) => todo!(),
+                ClientMessage::GetSounds(_) => todo!(),
+                ClientMessage::AdddSound(sound) => todo!(),
+                ClientMessage::RemoveSound(_) => todo!(),
+                ClientMessage::StopAlarm(i) => s.send(Alert::AlarmStopped(i)),
+            };
+
+            // Now that the receive has come through and the client is waiting on the server's send, do
+            // it. (`.get_mut()` is to get the sender, `BufReader` doesn't implement a pass-through
+            // `Write`.)
+            write.write_all(b"Hello from server!\n").unwrap();
+
+            // Print out the result, getting the newline for free!
+
+            // Clear the buffer so that the next iteration will display new data instead of messages
+            // stacking on top of one another.
+            buffer.clear();
+        });
+    }
+    Ok(())
+}
